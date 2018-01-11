8e887e567587c5e0710d1e984aee6d565b770c80
tm-roamer <tm165822@163.com>
2018-01-11 22:50:06 +0800

    格式化gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -19,25 +19,3 @@ gulp.task('default', function() {
 		.pipe(uglify())
 		.pipe(gulp.dest('./website/assets/pluginunit/ag-graph'));
 });
-
-
-
-// gulp.task('default', function() {
-// 	gulp.src(['./src/module.js'])
-// 		.pipe(fileinclude({
-// 			prefix: '@@',
-// 			basepath: '@file'
-// 		}))
-// 		.pipe(esformatter({indent: {value: '    '}}))
-// 		.pipe(rename(function (path) {
-// 			// path.dirname += "/ciao";
-// 			path.basename = package.name;
-// 		}))
-// 		.pipe(gulp.dest('./docs'))
-// 		.pipe(rename(function (path) {
-// 			path.basename = package.name;
-// 			path.extname = ".min.js"
-// 		}))
-// 		.pipe(uglify())
-// 		.pipe(gulp.dest('./docs'));
-// });
